00ed1b1eb48b564b159660977c19888d49697516
aabarcam <aeduardo10x@gmail.com>
2025-06-08 18:38:41 -0400

    feat(index): add view pagination

--- app/controllers/cat_facts_controller.rb
+++ app/controllers/cat_facts_controller.rb
@@ -2,6 +2,9 @@ class CatFactsController < ApplicationController
   def index
     facts = CatFactsService.new.get_page(params[:page])
     data = facts[:data]
-    @cat_facts = data.map { |element| element["fact"] }
+    @cat_facts = data.map { |element| element[:fact] }
+    puts facts[:current_page]
+    @current_page = facts[:current_page]
+    @last_page = facts[:last_page]
   end
 end
