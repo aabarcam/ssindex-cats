4118d9a05e00dce3d418da7649dcf3288e4b04d7
aabarcam <aeduardo10x@gmail.com>
2025-06-08 23:09:23 -0400

    test: add user model tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,22 @@
 require "test_helper"
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "invalid user without username" do
+    user = User.new()
+    assert_not user.valid?
+  end
+
+  test "valid user" do
+    count = User.count.to_s
+    user = User.new(username: "johndoe" + count)
+    assert user.valid?
+  end
+
+  test "invalid repeated user" do
+    count = User.count.to_s
+    user1 = User.new(username: "johndoe" + count)
+    user1.save
+    user2 = User.new(username: "johndoe" + count)
+    assert_not user2.valid?
+  end
 end
