311dce1c1d0438b52b3a2ce293666347a0dfc314
aabarcam <aeduardo10x@gmail.com>
2025-06-09 02:46:26 -0400

    style: delete comments

--- app/models/user_likes_cat_fact.rb
+++ app/models/user_likes_cat_fact.rb
@@ -1,9 +1,6 @@
 class UserLikesCatFact < ApplicationRecord
-  # has_secure_password
   has_many :users
   validates_associated :users
   validates :user_id, :fact_id, presence: true
   validates :fact_id, comparison: { greater_than: 0 }
-
-  # normalizes :email_address, with: ->(e) { e.strip.downcase }
 end
