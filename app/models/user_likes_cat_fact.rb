c82da83d4554b8b767de29d5308a41f06d29dcd0
aabarcam <aeduardo10x@gmail.com>
2025-06-09 12:11:16 -0400

    feat(db): validate user fact pair unique

--- app/models/user_likes_cat_fact.rb
+++ app/models/user_likes_cat_fact.rb
@@ -3,4 +3,5 @@ class UserLikesCatFact < ApplicationRecord
   validates_associated :users
   validates :user_id, :fact_id, presence: true
   validates :fact_id, comparison: { greater_than: 0 }
+  validates_uniqueness_of :user_id, scope: :fact_id
 end
