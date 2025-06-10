f6a6be9b7d01e1219995004ea60a37902a75ae29
aabarcam <aeduardo10x@gmail.com>
2025-06-09 21:53:16 -0400

    feat(views): popular facts view

---
+++ app/controllers/popular_facts_controller.rb
@@ -0,0 +1,12 @@
+class PopularFactsController < ApplicationController
+  def index
+    popular_fact_count = UserLikesCatFact.
+                      group(:fact_id).
+                      order("COUNT(fact_id) DESC").
+                      limit(10).count
+    popular_fact_ids = popular_fact_count.keys
+    facts = CatFactsService.new.get_facts popular_fact_ids
+    facts.each { |fact| fact[:likes] = popular_fact_count[fact[:id]] }
+    @popular_facts = facts.sort { |a, b| b[:likes] <=> a[:likes] }
+  end
+end
