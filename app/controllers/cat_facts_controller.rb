2cfa1ea048efdb6cca5f44dfcdfa301b3f0e025e
aabarcam <aeduardo10x@gmail.com>
2025-06-11 14:03:30 -0400

    style: trailing whitespaces

--- app/controllers/cat_facts_controller.rb
+++ app/controllers/cat_facts_controller.rb
@@ -2,8 +2,8 @@ class CatFactsController < ApplicationController
   def index
     begin
       data = CatFactsService.new.get_page(params[:page])
-      facts_array = CatFactsService.new.attach_id_to_fact(data[:data], 
-                                                          data[:current_page], 
+      facts_array = CatFactsService.new.attach_id_to_fact(data[:data],
+                                                          data[:current_page],
                                                           data[:per_page])
       fact_hash = { facts: facts_array,
                     current_page: data[:current_page],
