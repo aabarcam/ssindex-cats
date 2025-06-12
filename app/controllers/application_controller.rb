5bb55dc3586b6c1ce85c120faa7e90b5c81848ec
aabarcam <aeduardo10x@gmail.com>
2025-06-11 20:35:39 -0400

    feat(auth): jwt token encode and decode

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,34 @@
 class ApplicationController < ActionController::Base
-  include Authentication
+  # include Authentication
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+  before_action :authorized
+
+    def current_user
+      if decoded_token
+        user_id = decoded_token[0]["user_id"]
+        @user = User.find_by(id: user_id)
+      end
+    end
+
+    def encode_token(payload)
+      JWT.encode(payload, "secret_key")
+    end
+
+    def decoded_token
+      header = request.headers["Authorization"]
+      if header
+        begin
+          JWT.decode(header, "secret_key")
+        rescue JWT::DecodeError
+          nil
+        end
+      end
+    end
+
+    def authorized
+      unless !!current_user
+      render json: { message: "Please log in" }, status: :unauthorized
+      end
+    end
 end
